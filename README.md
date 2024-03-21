da62f4d174ad00bc548a416b36e516b338be90d8
Fasih-Haider01 <fasihhaider105@gmail.com>
2024-03-21 16:25:59 -0700

    my first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# simple-calculator
+ a simple calculator performing basic operations
---
+++ simple.ts
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "enter first number", type: "number", name: "first_number" },
+  { message: "enter second  number", type: "number", name: "second_number" },
+  {
+    message: "select the operator to perform the specific operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+  },
+]);
+//Statements
+if (answer.operator === "Addition") {
+  console.log(answer.first_number + answer.second_number);
+} else if (answer.operator === "Subtraction") {
+  console.log(answer.first_number - answer.second_number);
+} else if (answer.operator === "Multiplication") {
+  console.log(answer.first_number * answer.second_number);
+} else if (answer.operator === "Division") {
+  console.log(answer.first_number / answer.second_number);
+} else {
+  console.log("Please select a valid operator");
+}
